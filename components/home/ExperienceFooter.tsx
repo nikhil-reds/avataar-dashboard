d6a9f2b4402832f951b805122a6a9a5a6d00e74a
niks-nikhil-anand <niks.anand.developer@gmail.com>
2026-09-23 12:19:33 +0530

    feat: add disconnect control option to AvatarPanel, update home typography/layout, and add ripple styles for TapToTalkControl

--- components/home/ExperienceFooter.tsx
+++ components/home/ExperienceFooter.tsx
@@ -11,8 +11,8 @@ export function ExperienceFooter() {
         priority
         className="pointer-events-none absolute left-[4.259%] top-[97.865%] z-20 h-px w-[91.389%] max-w-none select-none"
       />
-      <p className="pointer-events-none absolute left-[27.5%] top-[97.344%] z-20 w-[44.907%] whitespace-nowrap text-center font-['Inter',system-ui,sans-serif] text-[clamp(0.54rem,1.481cqw,1rem)] font-normal uppercase leading-normal tracking-[0.58em] text-white">
-        Initialising Experience • 01
+      <p className="pointer-events-none absolute left-1/2 top-[97.344%] z-20 -translate-x-1/2 whitespace-nowrap pl-[0.58em] text-center font-['Inter',system-ui,sans-serif] text-[clamp(0.54rem,1.481cqw,1rem)] font-normal uppercase leading-normal tracking-[0.58em] text-white">
+        Initialising Experience
       </p>
     </>
   );
